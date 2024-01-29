74223cde13eb2a966df479c9995e75950b96edbb
webrian <webrian@gmx.net>
2024-01-29 17:42:23 +0100

    Update README and SQL Views

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-Umbenennen des runtergeladenen Tabelle
+* Runterladen der Tabelle mit den Resultaten von Google Drive als CSV
+* Umbenennen der runtergeladenen Tabelle
 
 ```
 cp Abstimmung\ Förderprojekte\ 2024\ \(Antworten\)\ -\ Formularantworten\ 1.csv abstimmung_gv_2024.csv
 ```
 
+* Löschen der ersten (leeren) Zeile in der CSV Tabelle
 * Speichern der Excel Mitgliederliste als CSV mit Komma als Trennung und Hochkommata für Textfelder
-* Umbenennund der CSV Liste
+* Umbenennen der CSV Liste
 
 ```
 mv "QGIS Usergruppe Schweiz Mitgliederliste.csv" mitgliederliste.csv
@@ -14,10 +16,10 @@ mv "QGIS Usergruppe Schweiz Mitgliederliste.csv" mitgliederliste.csv
 * Setzen der Verbindung zur Datenbank als Umgebungsvariablen
 
 ```
-export $DBNAME=your_db_name
-export $HOST=localhost
-export $PORT=5432
-export $USER=your_db_user
+export DBNAME=your_db_name
+export HOST=localhost
+export PORT=5432
+export USER=your_db_user
 ```
 
 * Kopiere die Tabelle Mitgliederliste in die Datenbank, überschreibe bestehende Daten
@@ -30,4 +32,22 @@ ogr2ogr -f PostgreSQL -oo HEADERS=YES -lco OVERWRITE=YES -lco SCHEMA=gv2024 PG:"
 
 ```
 ogr2ogr -f PostgreSQL -oo HEADERS=YES -lco OVERWRITE=YES -lco SCHEMA=gv2024 PG:"dbname=$DBNAME host=$HOST port=$PORT user=$USER" abstimmung_gv_2024.csv abstimmung_gv_2024
+```
+
+* Anlegen der Datenbank Views:
+
+```
+psql -h $HOST -p $PORT -f create_views.sql $DBNAME $USER
+```
+
+* Anzeigen Resultat
+
+```
+psql -h $HOST -p $PORT -c "SELECT * FROM gv2024.resultate_nach_projekt" $DBNAME $USER
+```
+
+* Export nach CSV
+
+```
+psql --csv -h $HOST -p $PORT -c "SELECT * FROM gv2024.resultate_nach_projekt" $DBNAME $USER > resultate.csv
 ```
--- create_views.sql
+++ create_views.sql
@@ -18,16 +18,25 @@ CREATE OR REPLACE VIEW gv2024.gewichtung_kategorie AS (
     AS t (id, kategorie, gewicht)
 );
 
+DROP VIEW IF EXISTS gv2024.projekte CASCADE;
+
 CREATE OR REPLACE VIEW gv2024.projekte AS (
     SELECT
         *
     FROM (
         VALUES
-            ( 1 , 'Projekt 1', 3000 ),        
-            ( 2 , 'Projekt 1', 2222 ),        
-            ( 3 , 'Projekt 1', 1333 ),
-            ( 4 , 'Projekt 1', 1222 ),
-            ( 5 , 'Projekt 1', 199 )
+            ( 1 , 'Linear Referencing Symbology and Labeling', 9200 ),
+            ( 2 , 'Annotations: Text on Line/Bezier Curves', 2880 ),
+            ( 3 , 'Quantized Mesh Format Provider (primarily for terrain)', 8840 ),
+            ( 4 , 'Profile Plot basierend auf swissalti3D / swisstopo API', 5760 ),
+            ( 5 , 'File Upload for Processing', 2880 ),
+            ( 6 , 'Unzip / Zip for Processing', 2880 ),
+            ( 7 , 'Store Python expression functions to project', 2160 ),
+            ( 8 , 'Store processing (Python) script algorithms to project', 2160 ),
+            ( 9 , 'Trusted projects / folders', 4320 ),
+            ( 10 , 'QField: Skizzenfunktion', 5760 ),
+            ( 11 , 'Stacked Diagrams', 8640 ),
+            ( 12 , 'Maintenance SwissLocator', 4320 )
     )
     AS t (projekt_id, beschreibung, preis_in_chf)
 );
@@ -39,17 +48,26 @@ CREATE VIEW gv2024.resultate_formatiert AS (
         ogc_fid,
         to_timestamp(zeitstempel,'DD.MM.YYYY HH24:MI:SS') AS zeitstempel,
         e_mail_adresse,
-        "ausgewählte förderprojekte",
-        split_part(split_part(ag."ausgewählte förderprojekte",',',1),':',1)::integer AS projekt_wahl_1,
-        split_part(split_part(ag."ausgewählte förderprojekte",',',2),':',1)::integer AS projekt_wahl_2,
-        split_part(split_part(ag."ausgewählte förderprojekte",',',3),':',1)::integer AS projekt_wahl_3
+        "wahl förderprojekte _ vote projets de soutien" AS wahl_vote,
+        split_part(split_part(ag."wahl förderprojekte _ vote projets de soutien",',',1),':',1)::integer AS projekt_wahl_1,
+        split_part(split_part(ag."wahl förderprojekte _ vote projets de soutien",',',2),':',1)::integer AS projekt_wahl_2,
+        split_part(split_part(ag."wahl förderprojekte _ vote projets de soutien",',',3),':',1)::integer AS projekt_wahl_3
     FROM
         gv2024.abstimmung_gv_2024 ag
 );
 
-DROP VIEW IF EXISTS gv2024.resultate_gewichtet;
+DROP VIEW IF EXISTS gv2024.resultate_gewichtet CASCADE;
 
 CREATE VIEW gv2024.resultate_gewichtet AS (
+    WITH neuste_stimmabgabe AS (
+        SELECT
+            MAX(rf.zeitstempel) AS zeitstempel,
+            rf.e_mail_adresse
+        FROM
+            gv2024.resultate_formatiert rf
+        GROUP BY
+            rf.e_mail_adresse
+    )
     SELECT
         rf.ogc_fid,
         rf.zeitstempel,
@@ -75,7 +93,52 @@ CREATE VIEW gv2024.resultate_gewichtet AS (
             WHEN rf.projekt_wahl_2 = 3 THEN gk.gewicht
             WHEN rf.projekt_wahl_3 = 3 THEN gk.gewicht
             ELSE 0::integer
-        END AS projekt_3_stimme
+        END AS projekt_3_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 4 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 4 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 4 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_4_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 5 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 5 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 5 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_5_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 6 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 6 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 6 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_6_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 7 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 7 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 7 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_7_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 8 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 8 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 8 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_8_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 9 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 9 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 9 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_9_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 10 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 10 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 10 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_10_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 11 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 11 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 11 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_11_stimme,
+        CASE WHEN rf.projekt_wahl_1 = 12 THEN gk.gewicht
+            WHEN rf.projekt_wahl_2 = 12 THEN gk.gewicht
+            WHEN rf.projekt_wahl_3 = 12 THEN gk.gewicht
+            ELSE 0::integer
+        END AS projekt_12_stimme
     FROM
         gv2024.resultate_formatiert rf 
     JOIN
@@ -86,6 +149,10 @@ CREATE VIEW gv2024.resultate_gewichtet AS (
         gv2024.gewichtung_kategorie gk
         ON
             gk.kategorie = ml.kategorie
+    JOIN
+        neuste_stimmabgabe ns
+        ON
+            rf.zeitstempel = ns.zeitstempel AND rf.e_mail_adresse = ns.e_mail_adresse
 );
 
 DROP VIEW IF EXISTS gv2024.resultate_nach_projekt;
@@ -109,6 +176,60 @@ CREATE VIEW gv2024.resultate_nach_projekt AS (
             SUM(projekt_3_stimme) AS anzahl_stimmen
         FROM
             gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            4::integer AS projekt,
+            SUM(projekt_4_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            5::integer AS projekt,
+            SUM(projekt_5_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            6::integer AS projekt,
+            SUM(projekt_6_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            7::integer AS projekt,
+            SUM(projekt_7_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            8::integer AS projekt,
+            SUM(projekt_8_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            9::integer AS projekt,
+            SUM(projekt_9_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            10::integer AS projekt,
+            SUM(projekt_10_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            11::integer AS projekt,
+            SUM(projekt_11_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
+        UNION
+            SELECT
+            12::integer AS projekt,
+            SUM(projekt_12_stimme) AS anzahl_stimmen
+        FROM
+            gv2024.resultate_gewichtet rg
     )
     SELECT
         p.projekt_id,
@@ -125,6 +246,17 @@ CREATE VIEW gv2024.resultate_nach_projekt AS (
         anzahl_stimmen
         DESC
 );
+
+-- Zeige ungültige Stimmabgaben
+SELECT
+    * 
+FROM 
+    gv2024.resultate_formatiert rf 
+WHERE
+    rf.e_mail_adresse NOT IN (
+        SELECT email FROM gv2024.mitgliederliste m
+    )
+;
 
 
 END;
